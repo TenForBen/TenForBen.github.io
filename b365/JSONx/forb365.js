251cf8ab3fd9be5c40833a9390b79f5594daa987
TenForBen <lampard.sibin102@gmail.com>
2019-02-27 03:35:53 +0530

    js addition 2

--- b365/JSONx/forb365.js
+++ b365/JSONx/forb365.js
@@ -10,13 +10,22 @@ function SRD(wk,j)
 	    //counter=counter+1;
 	    //ref.Tag[3].Bets.length
 	    		var totBets=wk.Tag[list].Bets.length
+	    		var rew=0
 	    		for(i=0;i<totBets;i++)
 	    				{
 							counter=wk.Tag[list].Bets[i].wager+counter
+							var res=wk.Tag[list].Bets[i].Result
+							if(res=="L")
+							{
+								rew=rew-wk.Tag[list].Bets[i].wager
+							}
+							else
+								rew=rew+wk.Tag[list].Bets[i].Reward
 
 	    				}
 	    				//console.log(counter)
 	    				$("#e")[0].value=counter
+	    				$("#r")[0].value=rew
 
 
 
