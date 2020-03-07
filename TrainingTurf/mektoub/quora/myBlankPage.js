188aa17b9b7965e2d81a6f5dfc2e74c80c7080dc
TenForBen <lampard.sibin102@gmail.com>
2020-03-07 20:38:29 +0100

    jquery view edit daily commit day acht-7

---
+++ TrainingTurf/mektoub/quora/myBlankPage.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function()
+{
+  //onclick="maxmillianAnna()"
+  $( "#LemonH1" ).click(function() {
+  		$( "#kadal" ).toggle( "slow" );
+	});
+});
+
+function maxmillianAnna()
+{
+		$( "#LemonH1" ).click(function() {
+  		$( "#kadal" ).toggle( "slow" );
+	});
+
+}
+
