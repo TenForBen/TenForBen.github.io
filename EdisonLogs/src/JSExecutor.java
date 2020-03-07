f512dd6832ae5cfa916a156d95bd94fc02cf17a8
TenForBen <lampard.sibin102@gmail.com>
2020-03-07 21:53:15 +0100

    javac view edit daily commit day acht-08

---
+++ EdisonLogs/src/JSExecutor.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class JSExecutor {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("https://tenforben.github.io/TrainingTurf/mektoub/quora/WhenYouHaveALemon.html");
+		WebElement ele= driver.findElement(By.id("LemonH1"));
+		String bgcolor= ele.getCssValue("backgroundColor");
+		
+		for(int i=0;i<100;i++) {
+			changecolor("rgb(0,200,0)",ele, driver);
+			changecolor(bgcolor,ele,driver);
+		}
+		clickele(ele, driver);
+		
+		
+	}
+public static void clickele(WebElement ele, WebDriver driver) {
+	JavascriptExecutor js= ((JavascriptExecutor) driver);
+	js.executeScript("arguments[0].click();", ele);
+}
+public static void changecolor (String color, WebElement ele,WebDriver driver) {
+	JavascriptExecutor js= ((JavascriptExecutor) driver);
+	js.executeScript("arguments[0].style.backgroundColor= '"+color+"'", ele);
+}
+}
