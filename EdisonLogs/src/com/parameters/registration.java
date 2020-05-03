2307d2bcc0ef07890222dad21ae093a6f918afdd
TenForBen <lampard.sibin102@gmail.com>
2020-05-03 17:24:35 +0200

    xcel check

--- EdisonLogs/src/com/parameters/registration.java
+++ EdisonLogs/src/com/parameters/registration.java
@@ -3,13 +3,21 @@ package com.parameters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+//import validationofdifferenttest;
+
+
 
 
 import com.test.utility.Xls_Reader;
 
+import validationofdifferenttestcases.frameworktest;
+
 public class registration {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException
+	
+	{
+		frameworktest fwt = new frameworktest();
 		
 		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
@@ -24,19 +32,23 @@ public class registration {
 		int rowcount= reader.getRowCount("testdata"); // passing sheet name
 		reader.addColumn("testdata", "status"); 
 		
-		for (int rownum=2; rownum<=rowcount;rownum++) {		
-		String username= reader.getCellData("testdata", "username", rownum);
-		String password= reader.getCellData("testdata", "password", rownum);
-		//webelement
-		driver.findElement(By.id("u")).clear();
-		driver.findElement(By.id("u")).sendKeys(username);
-		Thread.sleep(2000);
-		driver.findElement(By.id("p")).clear();
-		driver.findElement(By.id("p")).sendKeys(password);
-		Thread.sleep(2000);
-		System.out.println("The login details " +username);
-		reader.setCellData("testdata", "status", rownum, "pass");
+		for (int rownum=2; rownum<=rowcount;rownum++) 
+		{		
+			String username= reader.getCellData("testdata", "username", rownum);
+			String password= reader.getCellData("testdata", "password", rownum);
+			//webelement
+			driver.findElement(By.id("u")).clear();
+			driver.findElement(By.id("u")).sendKeys(username);
+			Thread.sleep(2000);
+			driver.findElement(By.id("p")).clear();
+			driver.findElement(By.id("p")).sendKeys(password);
+			Thread.sleep(2000);
+			System.out.println("The login details " +username);
+			String passer= ("text " + rownum);
+			reader.setCellData("testdata", "status", rownum, passer);
 		}
+		fwt.quitbrowser(driver);
+		
 	}
 
 }
