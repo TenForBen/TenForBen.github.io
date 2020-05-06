1088b3c5a0d9db2de2f63e511815ee52b9d6096d
TenForBen <lampard.sibin102@gmail.com>
2020-05-07 00:28:54 +0200

    success b4 cleanUp

--- EdisonLogs/src/validationofdifferenttestcases/gpsVier.java
+++ EdisonLogs/src/validationofdifferenttestcases/gpsVier.java
@@ -18,7 +18,7 @@ import validationofdifferenttestcases.frameworktest;
 public class gpsVier {
 
 
-	public  void gpsExcel()throws InterruptedException
+	public  String  gpsExcel(String place)throws InterruptedException
 	
 	{
 		frameworktest fwt = new frameworktest();
@@ -27,42 +27,45 @@ public class gpsVier {
 		WebDriver driver= new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();		
-		Xls_Reader reader= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\gps.xlsx");
+		//Xls_Reader reader= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\gps.xlsx");
 		//int rowcount= reader.getRowCount("testdata"); // passing sheet name
 		//reader.addColumn("testdata", "status"); //sunBath,20 min lunch, Mixy , mysteryDeutsch caller, 2'oclock strom. taxConsultant, band news., 10k steps, 5k run, near paralysys feel, new routes , naidu video call
-		int  LR =  reader.getLastRwoNum("Sheet1");
-		System.out.println("The last row by method  " + LR);
-		int LRs=LR+1;
-		System.out.println("The last row count is  " + LRs);
-		String place =reader.getCellData("Sheet1", "Places", LRs);		
+	//	int  LR =  reader.getLastRwoNum("Sheet1");
+		//System.out.println("The last row by method  " + LR);
+		//int LRs=LR+1;
+		//System.out.println("The last row count is  " + LRs);
+		//String place =reader.getCellData("Sheet1", "Places", LRs);		
 		String searchParam=place +" coordinates";		
 		String uri= "https://www.google.com/search?q=" + place +"+coordinates&aqs=chrome..69i57j0l4.8570j0j9&sourceid=chrome&ie=UTF-8";
 		System.out.println("URL formed -" +uri);
-		String url ="https://tenforben.github.io/Movies/HoMe.html";
 		driver.get(uri);
 		driver.findElement(By.xpath("//*[@id=\"Rzn5id\"]/div/a[2]")).click(); 
 		System.out.println("Clicked on makeshift  link" );		
 		String searchResult= driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div[1]/div[1]/div[1]/div/div[2]/div/div/div/div[1]	")).getText() ;
-		System.out.println("coordinates for "+ place  +  " are "+searchResult);
-		reader.setCellData("Sheet1", "Coordinates", LRs, searchResult);
+		//System.out.println("coordinates for "+ place  +  " are "+searchResult);
+		
+		//reader.setCellData("Sheet1", "Coordinates", LRs, searchResult);
 		System.out.println("coordinates updated in excel  ");		
 		fwt.quitbrowser(driver);
+		return searchResult;
 		
 	}
 	@Test
-	public void iteraetor()
+	public void iteraetor() throws InterruptedException
 	{
-		Xls_Reader reader= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\gps.xlsx");
-		//int rowcount= reader.getRowCount("testdata"); // passing sheet name
-		//reader.addColumn("testdata", "status"); //sunBath,20 min lunch, Mixy , mysteryDeutsch caller, 2'oclock strom. taxConsultant, band news., 10k steps, 5k run, near paralysys feel, new routes , naidu video call
-		int  LR =  reader.getLastRwoNum("Sheet1");
+		Xls_Reader r= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\gps.xlsx");
+		int  LR =  r.getLastRwoNum("Sheet1");
 		System.out.println("The last row by method  " + LR);
 		int LRs=LR+1;
 		System.out.println("The last row count is  " + LRs);
-		for( int i =1;i<=LRs;i++)
+		for( int i =1;i<=2;i++)
 		{
-			String place =reader.getCellData("Sheet1", "Places", i);	
-			System.out.println("Places  at position -"+ i +" is " + place);
+			String place =r.getCellData("Sheet1", "Places", i+1);	
+			System.out.println("Places  at position -"+ i +1+" is " + place);
+			String SR=gpsExcel(place);
+			r.setCellData("Sheet1", "Coordinates", i+1, SR);
+			System.out.println("coordinates updated in excel  ");		
+			
 		}
 		
 	}
