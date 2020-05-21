64dd88f35d7cc0e50228c4af9ccfcaa716cd5038
TenForBen <lampard.sibin102@gmail.com>
2020-05-21 23:59:23 +0200

    operaSOccer xcel 2 json

---
+++ AufWiedersehen/db/operaSoccer.js
@@ -0,0 +1,140 @@
+var OpSO=
+[
+				{
+					"newsId": "1099587",
+					"NewsHeadline": "Neymar will stay at PSG despite Barcelona interest, says former agent"
+				}, {
+					"newsId": "1099588",
+					"NewsHeadline": "Sainz hopes Alonso makes Formula One return"
+				}, {
+					"newsId": "1099589",
+					"NewsHeadline": "On this day in sport: Barcelona´s maiden European Cup triumph and Del Piero´s Juve farewell"
+				}, {
+					"newsId": "1099590",
+					"NewsHeadline": "Rangers would prefer to hold second spring training at new stadium"
+				}, {
+					"newsId": "1099591",
+					"NewsHeadline": "Walters open to Inglis making Maroons comeback"
+				}, {
+					"newsId": "1099592",
+					"NewsHeadline": "Premier League links riled RB Leipzig forward Poulsen"
+				}, {
+					"newsId": "1099387",
+					"NewsHeadline": "Ronaldo goes to the next level in the Champions League – Chiellini"
+				}, {
+					"newsId": "1099388",
+					"NewsHeadline": "Coronavirus: NFL facilities to partially reopen on Tuesday, pending local regulations"
+				}, {
+					"newsId": "1099389",
+					"NewsHeadline": "Coronavirus: Neymar working hard as star looks forward to return"
+				}, {
+					"newsId": "1099390",
+					"NewsHeadline": "Former England and Arsenal defender Sansom hospitalised"
+				}, {
+					"newsId": "1099391",
+					"NewsHeadline": "Guardiola one of the best managers ever – Man City´s Bernardo Silva"
+				}, {
+					"newsId": "1099392",
+					"NewsHeadline": "Getafe striker Molina denies wrongdoing after match-fixing report"
+				}, {
+					"newsId": "1099087",
+					"NewsHeadline": "Mbappe wants prolific Ben Yedder honoured in top scorer trophy debate"
+				}, {
+					"newsId": "1099088",
+					"NewsHeadline": "Saints release veteran guard Warford"
+				}, {
+					"newsId": "1099089",
+					"NewsHeadline": "Wenger hoping Arteta can restore Arsenal´s culture"
+				}, {
+					"newsId": "1099087",
+					"NewsHeadline": "Mbappe wants prolific Ben Yedder honoured in top scorer trophy debate"
+				}, {
+					"newsId": "1099088",
+					"NewsHeadline": "Saints release veteran guard Warford"
+				}, {
+					"newsId": "1099089",
+					"NewsHeadline": "Wenger hoping Arteta can restore Arsenal´s culture"
+				}, {
+					"newsId": "1099090",
+					"NewsHeadline": "Coronavirus: Eintracht Frankfurt warn fans to stay away from stadium – or team will lose points"
+				}, {
+					"newsId": "1099091",
+					"NewsHeadline": "Coronavirus: MLB reportedly shortens 2020 draft from 40 rounds to five"
+				}, {
+					"newsId": "1099092",
+					"NewsHeadline": "Panthers agree to four-year deal with number seven overall pick Brown"
+				}, {
+					"newsId": "1099093",
+					"NewsHeadline": "Mbappe is capable of marking an era at Real Madrid – Emery"
+				}, {
+					"newsId": "1099094",
+					"NewsHeadline": "Barcelona want Neymar again, says former assistant Moreno"
+				}, {
+					"newsId": "1099095",
+					"NewsHeadline": "I threw my phone – Daniel James reveals anger at missing out on Leeds move"
+				}, {
+					"newsId": "1099096",
+					"NewsHeadline": "Coronavirus: NSW health minister tells NRL to ´stick to the deal´ over flu vaccine"
+				}, {
+					"newsId": "1099097",
+					"NewsHeadline": "On this day in sport: Man Utd appoint Moyes, Kerry packs a punch"
+				}, {
+					"newsId": "1099098",
+					"NewsHeadline": "Rumour Has It: Liverpool in pole position to sign Koulibaly"
+				}, {
+					"newsId": "1099099",
+					"NewsHeadline": "Diego Simeone has a job for life at Atletico, says Unai Emery"
+				}, {
+					"newsId": "1099100",
+					"NewsHeadline": "Coronavirus: Brady questions whether Premier League strugglers can afford to leave out stars"
+				}, {
+					"newsId": "1099101",
+					"NewsHeadline": "Coronavirus: Atletico Madrid reveal Renan Lodi at home as players return to training"
+				}, {
+					"newsId": "1099102",
+					"NewsHeadline": "Coronavirus: Ricciardo expects ´chaos´ when F1 returns"
+				}, {
+					"newsId": "1099103",
+					"NewsHeadline": "Chiellini on the attack: Balotelli deserved a slap, Melo ´a rotten apple´"
+				}, {
+					"newsId": "1099104",
+					"NewsHeadline": "Coronavirus: Joseph Parker hoping August fight in ´New Zealand bubble´ gets approval"
+				}, {
+					"newsId": "1099105",
+					"NewsHeadline": "´It´s fake news!´ - Napoli dismiss Getafe claims, deny chasing Cucurella"
+				}, {
+					"newsId": "1099106",
+					"NewsHeadline": "Ricciardo retains F1 world title belief despite Renault frustration"
+				}, {
+					"newsId": "1099107",
+					"NewsHeadline": "´Great entertainer´ Stokes clearly deserving of world´s best honour, says Ford"
+				}, {
+					"newsId": "1099108",
+					"NewsHeadline": "Coronavirus: Vuelta a Espana cancels plans for stages in Portugal"
+				}, {
+					"newsId": "1099109",
+					"NewsHeadline": "Coronavirus: To B or not to B - that is the big Premier League and EFL question"
+				}, {
+					"newsId": "1099110",
+					"NewsHeadline": "Umtiti injured in second session of Barcelona training return"
+				}, {
+					"newsId": "1099111",
+					"NewsHeadline": "Cannavaro sympathises with Van Dijk over Messi´s Ballon d´Or triumph"
+				}, {
+					"newsId": "1099112",
+					"NewsHeadline": "Ulsan hit four to set early K League standard"
+				}, {
+					"newsId": "1099113",
+					"NewsHeadline": "Pogba and Fernandes can be ´amazing´ pairing with ´compromise´, tips Neville"
+				}, {
+					"newsId": "1099114",
+					"NewsHeadline": "Coronavirus: Revitalised Hamilton reveals problem with closed-door F1 races"
+				}, {
+					"newsId": "1099115",
+					"NewsHeadline": "Tyson Fury teases ´massive news´ is on the way in Instagram video"
+				}, {
+					"newsId": "1099116",
+					"NewsHeadline": "It´s not normal! Poyet bemused by timing of Ziyech deal after Chelsea sign nobody in January"
+				}	
+
+]
