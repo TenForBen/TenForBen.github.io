59d7ba3db44645059814419c2f3d0ee88141556e
TenForBen <lampard.sibin102@gmail.com>
2020-05-22 22:12:06 +0200

    xlsmInNextCommit

--- EdisonLogs/src/authorQuotes/operaSoccer.java
+++ EdisonLogs/src/authorQuotes/operaSoccer.java
@@ -19,80 +19,8 @@ import com.test.utility.Xls_Reader;
 import validationofdifferenttestcases.frameworktest;
 
 //ciot-0001 to make the code adaptable to take from the last vacant row 
-public class operaSoccer {
-	
-
-	public  String  gpsExcel(String place)throws InterruptedException
-	
-	{
-		frameworktest fwt = new frameworktest();
-		
-		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe");
-		WebDriver driver= new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();		
-		String searchParam=place +" coordinates";		
-		String uri= "https://www.google.com/search?q=" + place +"+coordinates&aqs=chrome..69i57j0l4.8570j0j9&sourceid=chrome&ie=UTF-8";
-		System.out.println("URL formed -" +uri);
-		driver.get(uri);
-		driver.findElement(By.xpath("//*[@id=\"Rzn5id\"]/div/a[2]")).click(); 
-		System.out.println("Clicked on makeshift  link" );		
-		String searchResult= driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div[1]/div[1]/div[1]/div/div[2]/div/div/div/div[1]	")).getText() ;
-		System.out.println("coordinates updated in excel  ");		
-		fwt.quitbrowser(driver);
-		return searchResult;	
-		
-	}
-	
-	public void iteraetor() throws InterruptedException
-	{
-		Xls_Reader r= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\gps.xlsx");
-		int  LR =  r.getLastRwoNum("Sheet1");
-		System.out.println("The last row by method  " + LR);
-		int LRs=LR+1;
-		System.out.println("The last row count is  " + LRs);
-		for( int i =2;i<=LRs;i++)
-		{
-			String place =r.getCellData("Sheet1", "Places", i);	
-			System.out.println("Places  at position   "+ i +"   is " + place);
-			String SR=gpsExcel(place);                                                               // method call
-			r.setCellData("Sheet1", "Coordinates", i, SR);
-			System.out.println("coordinates updated in excel  ");	
-			 Date date = new Date();
-		       System.out.println(new Timestamp(date.getTime()));
-		       System.out.println( TimeStamp.getCurrentTime());
-		       //TimeStamp  ts = TimeStamp.getCurrentTime();
-		   	//r.setCellDataTS("Sheet1", "timeStamp", i, ts);
-			
-		}
-		
-	}
-
-	
-	public void ExcelCounter() throws InterruptedException
-	{
-		Xls_Reader r= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\operaSoccer.xlsx");
-		int  LR =  r.getLastRwoNum("Sheet1");
-		System.out.println("The last row by method  " + LR);
-		int LRs=LR+1;
-		System.out.println("The last row count is  " + LRs);
-		for( int i =2;i<=LRs;i++)
-		{
-			String place =r.getCellData("Sheet1", "Places", i);	
-			System.out.println("Places  at position -"+ i +" is " + place);
-			String SR=gpsExcel(place);                                                               // method call
-			r.setCellData("Sheet1", "Coordinates", i, SR);
-			System.out.println("coordinates updated in excel  ");	
-			 Date date = new Date();
-		       System.out.println(new Timestamp(date.getTime()));
-		       System.out.println( TimeStamp.getCurrentTime());
-		       //TimeStamp  ts = TimeStamp.getCurrentTime();
-		   	//r.setCellDataTS("Sheet1", "timeStamp", i, ts);
-			
-		}
-		
-	}
-	
+public class operaSoccer 
+{
 	public  String  bohraa(int place)throws InterruptedException
 
 	{
@@ -109,6 +37,8 @@ public class operaSoccer {
 		driver.get(uri);
 		String HeadLine= driver.findElement(By.xpath("(//div[@class='clearfix'])[5]/h1")).getText() ;
 		System.out.println("HEADLINE IS - " +HeadLine );
+		String DateofNews= driver.findElement(By.xpath("(//div[@class='clearfix'])[5]/p")).getText() ;
+		System.out.println("DateofNews  IS - " +DateofNews );
 		fwt.quitbrowser(driver);
 		return HeadLine;
 		/*driver.findElement(By.xpath("//*[@id=\"Rzn5id\"]/div/a[2]")).click(); 
@@ -119,7 +49,7 @@ public class operaSoccer {
 		//return searchResult;	
 		
 	}
-	@Test
+	
 	public void triggerere() throws InterruptedException
 	{
 		Xls_Reader r= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\operaSoccer.xlsx");
@@ -140,8 +70,32 @@ public class operaSoccer {
 			
 		}
 	}
+	//reverse checking -1099740 cd H:/\vsos/\TenForBen.github.io
+
+	@Test
+	public void AureviourTtriggerere() throws InterruptedException
+	{
+		Xls_Reader r= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\operaSoccer.xlsx");
+		int  LR =  r.getLastRwoNum("Sheet1");
+		System.out.println("The last row by method  " + LR);
+		int LRs=LR+2;
+		int starter=1099734; // this is for the newsId
+		System.out.println("New Row is at " + LRs);
+		int limiter= LRs+5; // this one is for rows
+		for(int i=LRs;i<=limiter;i++)
+		{
+			String innsbruck = bohraa(starter);
+			r.setCellData("Sheet1", "NewsHeadline", i, innsbruck);
+			//newsId
+			String str1 = Integer.toString(starter);
+			r.setCellData("Sheet1", "newsId", i,str1);
+			starter=starter-1; //
+			
+		}
+	}
 
 
+	
 }
 
 
