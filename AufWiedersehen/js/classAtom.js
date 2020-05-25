a79e7ee57a23b84e9c0a9b4d50c375dc039fe7a8
TenForBen <lampard.sibin102@gmail.com>
2020-05-25 22:54:36 +0200

    OpSo

--- AufWiedersehen/js/classAtom.js
+++ AufWiedersehen/js/classAtom.js
@@ -293,6 +293,44 @@ function boat()
         }
 
     }
+    function loader3() {
+        //document.getElementById('Tbag').style.display=='none';
+        //$("body").css("background-color","beige");
+        //document.getElementById("regtable").style.backgroundColor="white";
+        if (OpSo22ndMay) {
+            //wordsArray = JSON.parse(worter);
+            for (var i = 0; i < OpSo22ndMay.length; i++)
+             {
+                prepareTableCell2(OpSo22ndMay[i].newsId, OpSo22ndMay[i].NewsHeadline);
+
+               
+             }
+
+
+        }
+
+
+
+    }
+    function prepareTableCell2(firstName, lastName) 
+    {
+        var table = document.getElementById("regtable");
+        var row = table.insertRow();
+        var firstNameCell = row.insertCell(0);
+        var lastNameCell = row.insertCell(1);
+        
+        firstNameCell.innerHTML = firstName;
+        lastNameCell.innerHTML = lastName;
+        //lastNameCell.innerHTML = ;
+        //var eurl= "<a href="http://www.m.soccerway.mobi/?sport=soccer&page=news&view=article&news_id=1099740&localization_id=www" > sample link</a>";
+       // http://www.m.soccerway.mobi/?sport=soccer&page=news&view=article&news_id=1099740&localization_id=www
+       //<a href="http://www.m.soccerway.mobi/?sport=soccer&page=news&view=article&news_id=1099740&localization_id=www" > sample link</a>
+
+        //firstNameCell.colSpan = 2;
+        
+    }
+
+
 	function milano(){
 	
 				document.getElementById("fc").value="orange";
