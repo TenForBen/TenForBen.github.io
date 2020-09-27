340d4666b1974e005d4348fa1307de2bb4d3b296
TenForBen <lampard.sibin102@gmail.com>
2020-09-27 22:23:07 +0200

    antonio after match

--- cucumber/src/StepDefinition/Steps.java
+++ cucumber/src/StepDefinition/Steps.java
@@ -1,28 +1,42 @@
 package StepDefinition;		
 
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;		
+
 import cucumber.api.java.en.Given;		
 import cucumber.api.java.en.Then;		
 import cucumber.api.java.en.When;		
 
 public class Steps {				
 
-     
-    @Given("^Open the Firefox and launch the application$")				
+    WebDriver driver;			
+    		
+    @Given("^Open the Firefox and launch the application$")					
     public void open_the_Firefox_and_launch_the_application() throws Throwable							
     {		
-        System.out.println("This Step open the Firefox and launch the application.");					
+      // System.setProperty("webdriver.gecko.driver", "E://Selenium//Selenium_Jars//geckodriver.exe");					
+     //  driver= new FirefoxDriver();		
+    	System.out.println("This Step open the browser and launch the application.");					
+    	System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe"); // declaring the chrome driver locatoion
+		driver= new ChromeDriver();// initializing chrome driver
+       driver.manage().window().maximize();			
+       driver.get("http://demo.guru99.com/v4");					
     }		
 
     @When("^Enter the Username and Password$")					
     public void enter_the_Username_and_Password() throws Throwable 							
     {		
-       System.out.println("This step enter the Username and Password on the login page.");					
+    	System.out.println("This step enter the Username and Password on the login page.");
+    	driver.findElement(By.name("uid")).sendKeys("username12");							
+       driver.findElement(By.name("password")).sendKeys("password12");							
     }		
 
     @Then("^Reset the credential$")					
     public void Reset_the_credential() throws Throwable 							
-    {    		
-        System.out.println("This step click on the Reset button.");					
+    {		
+    	System.out.println("This step click on the Reset button.");		
+    	driver.findElement(By.name("btnReset")).click();					
     }		
-
-}
+}		
