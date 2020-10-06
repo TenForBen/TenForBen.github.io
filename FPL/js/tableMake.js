4815664e726acead53929297e6eeef3e000e0255
TenForBen <lampard.sibin102@gmail.com>
2020-10-06 22:02:03 +0200

    FPL - new format

--- FPL/js/tableMake.js
+++ FPL/js/tableMake.js
@@ -23,7 +23,6 @@ function tableShakers()
 
 		}	
 }
-
 function loader4()
 {
     if (s)
@@ -32,15 +31,10 @@ function loader4()
         for (var i = 0; i < s.length; i++)
          {
             prepareTableCell3(s[i].Teams,s[i].Player_1,s[i].Player_2,s[i].Player_3,s[i].Player_4,s[i].Player_5,s[i].Player_6,s[i].Player_7,s[i].Player_8,s[i].Player_9,s[i].Player_10,s[i].Player_11,s[i].Player_12,s[i].Player_13,s[i].Player_14,s[i].Player_15,s[i].SXL, s[i].Bench);
-
-          
          }
     }
-
     tableShakers();
-
 }
-
 function prepareTableCell3(Team, p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15, krups, stick) 
 {
     var table = document.getElementById("regtable");
@@ -83,5 +77,61 @@ function prepareTableCell3(Team, p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,
     pp15.innerHTML = p15;
     
 }
+function loader4mgw()
+{
+    if (s)
+     {
+        //wordsArray = JSON.parse(worter);
+        for (var i = 0; i < s.length; i++)
+         {
+            prepareTableCell3mgw(s[i].Teams,s[i].manager_Name,s[i].Player_1,s[i].Player_2,s[i].Player_3,s[i].Player_4,s[i].Player_5,s[i].Player_6,s[i].Player_7,s[i].Player_8,s[i].Player_9,s[i].Player_10,s[i].Player_11,s[i].Player_12,s[i].Player_13,s[i].Player_14,s[i].Player_15,s[i].SXL, s[i].Bench);
+         }
+    }
+    tableShakers();
+}
 
+function prepareTableCell3mgw(Team,mN,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15, krups, stick) 
+{
+    var table = document.getElementById("regtable");
+    var row = table.insertRow();
+    var teamName = row.insertCell(0);
+    var manager_Name = row.insertCell(1);
+    var lastNameCell = row.insertCell(2);
+    var pp1 = row.insertCell(3);
+    var pp2 = row.insertCell(4);
+    var pp3 = row.insertCell(5);
+    var pp4 = row.insertCell(6);
+    var pp5 = row.insertCell(7);
+    var pp6 = row.insertCell(8);
+    var pp7 = row.insertCell(9);
+    var pp8 = row.insertCell(10);
+    var pp9 = row.insertCell(11);
+    var pp10 = row.insertCell(12);
+    var pp11 = row.insertCell(13);
+    var pp12 = row.insertCell(14);
+    var pp13 = row.insertCell(15);
+    var pp14 = row.insertCell(16);
+    var pp15 = row.insertCell(17);
+    var stickI = row.insertCell(18);     
+    teamName.innerHTML = Team;
+    manager_Name.innerHTML = mN;
+    lastNameCell.innerHTML = krups;
+    stickI.innerHTML = stick;
+    pp1.innerHTML = p1;
+    pp2.innerHTML = p2;
+    pp3.innerHTML = p3;
+    pp4.innerHTML = p4;
+    pp5.innerHTML = p5;
+    pp6.innerHTML = p6;
+    pp7.innerHTML = p7;
+    pp8.innerHTML = p8;
+    pp9.innerHTML = p9;
+    pp10.innerHTML = p10;
+    pp11.innerHTML = p11;
+    pp12.innerHTML = p12;
+    pp13.innerHTML = p13;
+    pp14.innerHTML = p14;
+    pp15.innerHTML = p15;
+    
+}
     
