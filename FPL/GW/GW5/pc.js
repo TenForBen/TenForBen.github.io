c0d8f939e7ec28ee93eb651d7a72407e0645f946
TenForBen <lampard.sibin102@gmail.com>
2020-10-18 00:51:37 +0200

    pc gw5 added fpl home

---
+++ FPL/GW/GW5/pc.js
@@ -0,0 +1,331 @@
+var pcc =[{
+	"PLAYERS": "McCarthy",
+	"PLAYERS_COUNT": "10"
+}, {
+	"PLAYERS": "Dunne",
+	"PLAYERS_COUNT": "10"
+}, {
+	"PLAYERS": "Saïss",
+	"PLAYERS_COUNT": "13"
+}, {
+	"PLAYERS": "Castagne",
+	"PLAYERS_COUNT": "7"
+}, {
+	"PLAYERS": "Mitchell",
+	"PLAYERS_COUNT": "15"
+}, {
+	"PLAYERS": "Salah",
+	"PLAYERS_COUNT": "25"
+}, {
+	"PLAYERS": "Son",
+	"PLAYERS_COUNT": "20"
+}, {
+	"PLAYERS": "Zaha",
+	"PLAYERS_COUNT": "10"
+}, {
+	"PLAYERS": "Wilson",
+	"PLAYERS_COUNT": "4"
+}, {
+	"PLAYERS": "Calvert-Lewin",
+	"PLAYERS_COUNT": "16"
+}, {
+	"PLAYERS": "Jiménez",
+	"PLAYERS_COUNT": "10"
+}, {
+	"PLAYERS": "Martínez",
+	"PLAYERS_COUNT": "11"
+}, {
+	"PLAYERS": "De Bruyne",
+	"PLAYERS_COUNT": "4"
+}, {
+	"PLAYERS": "Douglas",
+	"PLAYERS_COUNT": "4"
+}, {
+	"PLAYERS": "Alzate",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Ryan",
+	"PLAYERS_COUNT": "7"
+}, {
+	"PLAYERS": "Coady",
+	"PLAYERS_COUNT": "4"
+}, {
+	"PLAYERS": "Justin",
+	"PLAYERS_COUNT": "11"
+}, {
+	"PLAYERS": "Taylor",
+	"PLAYERS_COUNT": "6"
+}, {
+	"PLAYERS": "Sterling",
+	"PLAYERS_COUNT": "7"
+}, {
+	"PLAYERS": "Barnes",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Podence",
+	"PLAYERS_COUNT": "5"
+}, {
+	"PLAYERS": "Werner",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "James",
+	"PLAYERS_COUNT": "9"
+}, {
+	"PLAYERS": "Davis",
+	"PLAYERS_COUNT": "4"
+}, {
+	"PLAYERS": "Chilwell",
+	"PLAYERS_COUNT": "4"
+}, {
+	"PLAYERS": "Ayling",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Grealish",
+	"PLAYERS_COUNT": "5"
+}, {
+	"PLAYERS": "Bamford",
+	"PLAYERS_COUNT": "10"
+}, {
+	"PLAYERS": "Kane",
+	"PLAYERS_COUNT": "14"
+}, {
+	"PLAYERS": "Steer",
+	"PLAYERS_COUNT": "7"
+}, {
+	"PLAYERS": "Bissouma",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Walker-Peters",
+	"PLAYERS_COUNT": "8"
+}, {
+	"PLAYERS": "Alexander-Arnold",
+	"PLAYERS_COUNT": "10"
+}, {
+	"PLAYERS": "Dier",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Rodríguez",
+	"PLAYERS_COUNT": "12"
+}, {
+	"PLAYERS": "Ferguson",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Gabriel",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Fernandes",
+	"PLAYERS_COUNT": "5"
+}, {
+	"PLAYERS": "Maupay",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Costa",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Ramsdale",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Keane",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Alderweireld",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Azpilicueta",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Callum",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Button",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Oriol",
+	"PLAYERS_COUNT": "5"
+}, {
+	"PLAYERS": "Tierney",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Schmeichel",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "van",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Lamptey",
+	"PLAYERS_COUNT": "4"
+}, {
+	"PLAYERS": "Saint-Maximin",
+	"PLAYERS_COUNT": "4"
+}, {
+	"PLAYERS": "Pulisic",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Ings",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Alioski",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Bernardo",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Robertson",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Mitrović",
+	"PLAYERS_COUNT": "6"
+}, {
+	"PLAYERS": "Gordon",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Foden",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Meslier",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Pereira",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Aubameyang",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Pickford",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Holding",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Reed",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Egan",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Jota",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Vardy",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Alisson",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Mount",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Macey",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Richarlison",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Patrício",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Digne",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Rashford",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Delap",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Watkins",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Forster",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Targett",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Anguissa",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Jorginho",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Nyland",
+	"PLAYERS_COUNT": "3"
+}, {
+	"PLAYERS": "Martin",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Stephens",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Lascelles",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Klich",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Aké",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Stevens",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Townsend",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Lundstram",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Fabio",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Söyüncü",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Carter-Vickers",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Pope",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Walker",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Mané",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Johnstone",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Bardsley",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Branthwaite",
+	"PLAYERS_COUNT": "2"
+}, {
+	"PLAYERS": "Bertrand",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Casey",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Phillips",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Guaita",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Soucek",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Trossard",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Dallas",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Bednarek",
+	"PLAYERS_COUNT": "1"
+}, {
+	"PLAYERS": "Wood",
+	"PLAYERS_COUNT": "1"
+}]
