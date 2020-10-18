c91d4d0239180e792a5c02abd54b23c76e5d3466
TenForBen <lampard.sibin102@gmail.com>
2020-10-18 16:07:07 +0200

    zaha penalti

--- FPL/js/tableMake.js
+++ FPL/js/tableMake.js
@@ -34,6 +34,8 @@ function loader4()
          }
     }
     tableShakers();
+    sortTable();
+
 }
 function prepareTableCell3(Team, p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15, krups, stick) 
 {
