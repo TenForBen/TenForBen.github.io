08386024e2484c5af6e3c2b13fb115321e88c4aa
TenForBen <lampard.sibin102@gmail.com>
2020-10-18 12:02:03 +0200

    automatic starting xl points sorter

--- FPL/js/tableMake.js
+++ FPL/js/tableMake.js
@@ -88,6 +88,11 @@ function loader4mgw()
          }
     }
     //tableShakers();
+    // sorter
+     sortTable_col3();
+
+
+
 }
 
 function prepareTableCell3mgw(Team,mN,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15, krups, stick) 
@@ -224,6 +229,41 @@ function sortTable() {
       one from current row and one from the next:*/
       x = rows[i].getElementsByTagName("TD")[1];
       y = rows[i + 1].getElementsByTagName("TD")[1];
+      //check if the two rows should switch place:
+      if (Number(x.innerHTML) < Number(y.innerHTML)) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
+function sortTable_col3() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("regtable");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[2];
+      y = rows[i + 1].getElementsByTagName("TD")[2];
       //check if the two rows should switch place:
       if (Number(x.innerHTML) < Number(y.innerHTML)) {
         //if so, mark as a switch and break the loop:
