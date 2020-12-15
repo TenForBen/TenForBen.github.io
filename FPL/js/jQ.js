1e8b33e9161a147b6ce1d3143d8bcdec82428c8d
TenForBen <lampard.sibin102@gmail.com>
2020-12-16 00:44:13 +0100

    some meaning full coding - sorter

---
+++ FPL/js/jQ.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+ $("#shit").click(function(){
+    $("#shit").toggleClass("maine");
+    sortTable();
+  });
+});
+
+$(document).ready(function(){
+ $("#Deepshit").click(function(){
+    $("#Deepshit").toggleClass("maine");
+    sortTable_col3();
+  });
+});
