e4cc76b778d111c83ba9568b6cb6ffcb4dcf8b89
TenForBen <lampard.sibin102@gmail.com>
2021-06-17 01:40:50 +0200

    GroceryList appended to 20

--- FPL/GW/GW25/pc4.js
+++ FPL/GW/GW25/pc4.js
@@ -66,16 +66,96 @@ var pcc = [
    {
       "itemID":"9",
       "ItemName":"KornerBalanceGreenLarge_L",
-      "dayOfPurchase":"TBD",
-      "expiryDate":"TBD",
+      "dayOfPurchase":"1st week of June",
+      "expiryDate":"19/06/2021",
       "consumedFlag":"TBD",
       "Comment":"NA"
    },
    {
       "itemID":"10",
       "ItemName":"KornerBalanceGreenLarge_S",
+      "dayOfPurchase":"2nd week of June",
+      "expiryDate":"TBD",
+      "consumedFlag":"TBD",
+      "Comment":"TBD"
+   },
+   {
+      "itemID":"11",
+      "ItemName":"Bikano Kaju Mixture",
+      "dayOfPurchase":"NA",
+      "expiryDate":"7/6/2021",
+      "consumedFlag":"1",
+      "Comment":"Consume Quickly"
+   },
+   {
+      "itemID":"12",
+      "ItemName":"Lays Carribean - Orange",
       "dayOfPurchase":"TBD",
       "expiryDate":"TBD",
+      "consumedFlag":"TBD",
+      "Comment":"TBD"
+   },
+   {
+      "itemID":"13",
+      "ItemName":"Lays American  - green",
+      "dayOfPurchase":"TBD",
+      "expiryDate":"TBD",
+      "consumedFlag":"TBD",
+      "Comment":"NA"
+   },
+   {
+      "itemID":"14",
+      "ItemName":"potata biscuit",
+      "dayOfPurchase":"TBD",
+      "expiryDate":"TBD",
+      "consumedFlag":"TBD",
+      "Comment":"TBD"
+   },
+   {
+      "itemID":"15",
+      "ItemName":"fruhlings Quark",
+      "dayOfPurchase":"2nd week of june",
+      "expiryDate":"21/06/2021",
+      "consumedFlag":"1",
+      "Comment":"NA"
+   },
+   {
+      "itemID":"16",
+      "ItemName":"Ananas Saft",
+      "dayOfPurchase":"1st week of June",
+      "expiryDate":"21/12/2021",
+      "consumedFlag":"TBD",
+      "Comment":"TBD"
+   },
+   {
+      "itemID":"17",
+      "ItemName":"Philadelphia schnittlauch",
+      "dayOfPurchase":"2nd week of June",
+      "expiryDate":"4/8/2021",
+      "consumedFlag":"TBD",
+      "Comment":"TBD"
+   },
+   {
+      "itemID":"18",
+      "ItemName":"Philadelphia milka",
+      "dayOfPurchase":"1st week of June",
+      "expiryDate":"4/9/2021",
+      "consumedFlag":"TBD",
+      "Comment":"TBD"
+   },
+   {
+      "itemID":"19",
+      "ItemName":"Pears",
+      "dayOfPurchase":"1st week of June",
+      "expiryDate":"20/06/2021",
+      "consumedFlag":"TBD",
+      "Comment":"TBD"
+   },
+   {
+      "itemID":"20",
+      "ItemName":"Banana",
+      "dayOfPurchase":"2nd week of June",
+      "expiryDate":"20/06/2021",
       "consumedFlag":"TBD",
       "Comment":"TBD"
    }
