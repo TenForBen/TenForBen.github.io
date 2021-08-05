0af1c668f5e4d7b1b58199c0859e31d5a0178263
TenForBen <lampard.sibin102@gmail.com>
2021-08-06 00:20:41 +0200

    zip logic added advisable - new directory

--- FPL/vannilaWeatherApp/fetch.js
+++ FPL/vannilaWeatherApp/fetch.js
@@ -2,10 +2,14 @@ class Fetch {
   async getCurrent(input) {
     const myKey = "39a9a737b07b4b703e3d1cd1e231eedc";
 
-    //make request to url
-
+    //make request to url 
+    // lesson learnt with yip query param.. units param dosent work..
+    //https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}&units=metric
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}&units=metric`
+       //`https://api.openweathermap.org/data/2.5/weather?zip=${input}&appid=39a9a737b07b4b703e3d1cd1e231eedc`
+
+      
     );
 
     const data = await response.json();
