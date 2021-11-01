165f3e3bee833a3b148ac6bc927df11d4b8ed1d1
TenForBen <lampard.sibin102@gmail.com>
2021-11-01 22:52:59 +0100

    GW 10 md3

--- EdisonLogs/src/scrappers/FPL_scrapper_advanched.java
+++ EdisonLogs/src/scrappers/FPL_scrapper_advanched.java
@@ -155,14 +155,14 @@ public class FPL_scrapper_advanched {
 	{
 		
 		Xls_Reader r= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\EdisonLogs\\weather.xlsx");
-		String snj ="FPL6";
+		String snj ="RoC";
 		System.out.println("League Scrapper - " +snj);	
 		int  LR =  r.getLastRwoNum(snj);
 		//System.out.println("The last row by method  " + LR);
 		int LRs=LR+1;
 		System.out.println("The last row count is LRs " + LRs);
 		int numVar = 2;
-		int gw=9;		
+		int gw=10;		
 		for( numVar =1;numVar<=1;numVar++)
 		{
 				for( int i =2;i<=LRs;i++)
