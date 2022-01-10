1ada37a709d65ae924cc8cb820520568c20e8f7e
TenForBen <lampard.sibin102@gmail.com>
2022-01-10 22:15:36 +0100

    profiling done

--- wiedersehen/weatherAppTest/ApiUiInt/src/test/java/openWeatherApp/ApiUiInt/apeeTest.java
+++ wiedersehen/weatherAppTest/ApiUiInt/src/test/java/openWeatherApp/ApiUiInt/apeeTest.java
@@ -38,5 +38,35 @@ public class apeeTest {
 		when().get("data/2.5/weather").
 		then().log().all().assertThat().statusCode(200);
 	}
+	
+	@Test
+	public void Metrics_zip ()
+	{
+		RestAssured.baseURI ="https://api.openweathermap.org";
+		given().log().all().
+		queryParam("zip", "110010,IN").queryParam("appid", "2b1fd2d7f77ccf1b7de9b441571b39b8").queryParam("units", "metric").
+		when().get("data/2.5/weather").
+		then().log().all().assertThat().statusCode(200);
+	}
+	@Test
+	public void Metrics_coords ()
+	{
+		RestAssured.baseURI ="https://api.openweathermap.org";
+		given().log().all().
+		queryParam("lat", "12").queryParam("lon", "79").queryParam("appid", "2b1fd2d7f77ccf1b7de9b441571b39b8").queryParam("units", "metric").
+		when().get("data/2.5/weather").
+		then().log().all().assertThat().statusCode(200);
+	}
+	
+	@Test
+	public void Metrics_locId ()
+	{
+		RestAssured.baseURI ="https://api.openweathermap.org";
+		given().log().all().
+		queryParam("id", "617094").queryParam("appid", "2b1fd2d7f77ccf1b7de9b441571b39b8").queryParam("units", "metric").
+		when().get("data/2.5/weather").
+		then().log().all().assertThat().statusCode(200);
+	}
+	
 
 }
