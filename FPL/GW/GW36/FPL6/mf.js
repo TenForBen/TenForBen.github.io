fd8de0e5f314bb0bee4d5c779b23aae0c3b7d4b9
TenForBen <lampard.sibin102@gmail.com>
2022-05-08 11:46:54 +0200

    gw 36 sunday

---
+++ FPL/GW/GW36/FPL6/mf.js
@@ -0,0 +1,282 @@
+var v =[
+   {
+      "Places":"Manfort",
+      "Coordinates":"Lat : 51 long : 7",
+      "timeStamp":"2022-54-02 12:54:57",
+      "Weather":"8.56",
+      "Country":"DE"
+   },
+   {
+      "Places":"Welschen-Ennest",
+      "Coordinates":"Lat : 51 long : 8",
+      "timeStamp":"2022-34-02 01:34:15",
+      "Weather":"5.39",
+      "Country":"DE"
+   },
+   {
+      "Places":"Herbelhausen",
+      "Coordinates":"Lat : 51 long : 9",
+      "timeStamp":"2022-39-02 01:39:24",
+      "Weather":"3.94",
+      "Country":"DE"
+   },
+   {
+      "Places":"Richelsdorf",
+      "Coordinates":"Lat : 51 long : 10",
+      "timeStamp":"2022-42-02 01:42:26",
+      "Weather":"3.83",
+      "Country":"DE"
+   },
+   {
+      "Places":"Gispersleben",
+      "Coordinates":"Lat : 51 long : 11",
+      "timeStamp":"2022-45-02 01:45:26",
+      "Weather":"4.71",
+      "Country":"DE"
+   },
+   {
+      "Places":"Wetterzeube",
+      "Coordinates":"Lat : 51 long : 12",
+      "timeStamp":"2022-48-02 01:48:24",
+      "Weather":"5.08",
+      "Country":"DE"
+   },
+   {
+      "Places":"Kockisch",
+      "Coordinates":"Lat : 51 long : 13",
+      "timeStamp":"2022-51-02 01:51:23",
+      "Weather":"4.13",
+      "Country":"DE"
+   },
+   {
+      "Places":"Mühlsdorf",
+      "Coordinates":"Lat : 51 long : 14",
+      "timeStamp":"2022-52-02 01:52:28",
+      "Weather":"4.13",
+      "Country":"DE"
+   },
+   {
+      "Places":"Višňova",
+      "Coordinates":"Lat : 51 long : 15",
+      "timeStamp":"2022-57-02 01:57:27",
+      "Weather":"3.85",
+      "Country":"CZ"
+   },
+   {
+      "Places":"Schönau am Katzbach",
+      "Coordinates":"Lat : 51 long : 16",
+      "timeStamp":"2022-00-02 02:00:26",
+      "Weather":"1.72",
+      "Country":"PL"
+   },
+   {
+      "Places":"Żórawina",
+      "Coordinates":"Lat : 51 long : 17",
+      "timeStamp":"2022-03-02 02:03:23",
+      "Weather":"4.07",
+      "Country":"PL"
+   },
+   {
+      "Places":"Konstadt",
+      "Coordinates":"Lat : 51 long : 18",
+      "timeStamp":"2022-02-02 02:02:04",
+      "Weather":"1.75",
+      "Country":"PL"
+   },
+   {
+      "Places":"Miedzno",
+      "Coordinates":"Lat : 51 long : 19",
+      "timeStamp":"2022-09-02 02:09:25",
+      "Weather":"0.87",
+      "Country":"PL"
+   },
+   {
+      "Places":"Słupia",
+      "Coordinates":"Lat : 51 long : 20",
+      "timeStamp":"2022-12-02 02:12:29",
+      "Weather":"0.54",
+      "Country":"PL"
+   },
+   {
+      "Places":"Starachowice",
+      "Coordinates":"Lat : 51 long : 21",
+      "timeStamp":"2022-15-02 02:15:27",
+      "Weather":"-0.35",
+      "Country":"PL"
+   },
+   {
+      "Places":"Dzierzkowice",
+      "Coordinates":"Lat : 51 long : 22",
+      "timeStamp":"2022-18-02 02:18:31",
+      "Weather":"-0.3",
+      "Country":"PL"
+   },
+   {
+      "Places":"Woiwodschaft Lublin",
+      "Coordinates":"Lat : 51 long : 23",
+      "timeStamp":"2022-21-02 02:21:25",
+      "Weather":"-0.23",
+      "Country":"PL"
+   },
+   {
+      "Places":"Dubienka",
+      "Coordinates":"Lat : 51 long : 24",
+      "timeStamp":"2022-24-02 02:24:25",
+      "Weather":"-1.51",
+      "Country":"PL"
+   },
+   {
+      "Places":"Oblast Wolhynien",
+      "Coordinates":"Lat : 51 long : 25",
+      "timeStamp":"2022-27-02 02:27:34",
+      "Weather":"-1.6",
+      "Country":"UA"
+   },
+   {
+      "Places":"Orzhiv",
+      "Coordinates":"Lat : 51 long : 26",
+      "timeStamp":"2022-32-02 02:32:23",
+      "Weather":"-1.62",
+      "Country":"UA"
+   },
+   {
+      "Places":"Sel’tse",
+      "Coordinates":"Lat : 51 long : 27",
+      "timeStamp":"1643765810",
+      "Weather":"-3.91",
+      "Country":"UA"
+   },
+   {
+      "Places":"Novi Bilokorovychi",
+      "Coordinates":"Lat : 51 long : 28",
+      "timeStamp":"1643840022",
+      "Weather":"0.09",
+      "Country":"UA"
+   },
+   {
+      "Places":"Chopovychi",
+      "Coordinates":"Lat : 51 long : 29",
+      "timeStamp":"1643840156",
+      "Weather":"0.14",
+      "Country":"UA"
+   },
+   {
+      "Places":"Pyrohovychi",
+      "Coordinates":"Lat : 51 long : 30",
+      "timeStamp":"1643840423",
+      "Weather":"0.23",
+      "Country":"UA"
+   },
+   {
+      "Places":"Ostër",
+      "Coordinates":"Lat : 51 long : 31",
+      "timeStamp":"1643840666",
+      "Weather":"-5.12",
+      "Country":"UA"
+   },
+   {
+      "Places":"Naumivs’ke",
+      "Coordinates":"Lat : 51 long : 32",
+      "timeStamp":"1643840907",
+      "Weather":"-4.53",
+      "Country":"UA"
+   },
+   {
+      "Places":"Ivan’kov",
+      "Coordinates":"Lat : 51 long : 33",
+      "timeStamp":"1643841146",
+      "Weather":"-0.95",
+      "Country":"UA"
+   },
+   {
+      "Places":"Oblast Sumy",
+      "Coordinates":"Lat : 51 long : 34",
+      "timeStamp":"1643841390",
+      "Weather":"-1.78",
+      "Country":"UA"
+   },
+   {
+      "Places":"Sumy",
+      "Coordinates":"Lat : 51 long : 35",
+      "timeStamp":"1643841625",
+      "Weather":"-1.06",
+      "Country":"UA"
+   },
+   {
+      "Places":"Peny",
+      "Coordinates":"Lat : 51 long : 36",
+      "timeStamp":"1643841863",
+      "Weather":"-2.66",
+      "Country":"RU"
+   },
+   {
+      "Places":"Mochaki",
+      "Coordinates":"Lat : 51 long : 37",
+      "timeStamp":"1643842105",
+      "Weather":"-2.95",
+      "Country":"RU"
+   },
+   {
+      "Places":"Ivanovka",
+      "Coordinates":"Lat : 51 long : 38",
+      "timeStamp":"1643842344",
+      "Weather":"-0.1",
+      "Country":"RU"
+   },
+   {
+      "Places":"Ostrogoschsk",
+      "Coordinates":"Lat : 51 long : 39",
+      "timeStamp":"1643842584",
+      "Weather":"-0.13",
+      "Country":"RU"
+   },
+   {
+      "Places":"Bobrov",
+      "Coordinates":"Lat : 51 long : 40",
+      "timeStamp":"1643842825",
+      "Weather":"0.3",
+      "Country":"RU"
+   },
+   {
+      "Places":"Yelan’-Kolenovskiy",
+      "Coordinates":"Lat : 51 long : 41",
+      "timeStamp":"1643843063",
+      "Weather":"0.09",
+      "Country":"RU"
+   },
+   {
+      "Places":"Uryupinsk",
+      "Coordinates":"Lat : 51 long : 42",
+      "timeStamp":"1643843303",
+      "Weather":"-0.01",
+      "Country":"RU"
+   },
+   {
+      "Places":"Kikvidze",
+      "Coordinates":"Lat : 51 long : 43",
+      "timeStamp":"1643843548",
+      "Weather":"-0.15",
+      "Country":"RU"
+   },
+   {
+      "Places":"Yelan'",
+      "Coordinates":"Lat : 51 long : 44",
+      "timeStamp":"1643843945",
+      "Weather":"0.13",
+      "Country":"RU"
+   },
+   {
+      "Places":"Zhirnovsk",
+      "Coordinates":"Lat : 51 long : 45",
+      "timeStamp":"1643844039",
+      "Weather":"0.49",
+      "Country":"RU"
+   },
+   {
+      "Places":"Krasnoarmeisk",
+      "Coordinates":"Lat : 51 long : 46",
+      "timeStamp":"1643844131",
+      "Weather":"0.47",
+      "Country":"RU"
+   }
+]
