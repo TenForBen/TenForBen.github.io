68c01f03c20aa65057fa31a1fd872bfa45dc85b6
TenForBen <lampard.sibin102@gmail.com>
2022-08-02 00:56:03 +0200

    breakthrough moment

---
+++ FPL/GW/GW38/FPL6/trip.js
@@ -0,0 +1,74 @@
+var s=[
+   {
+      "Stadt":"krakow",
+      "Coordinates":"Lat : 50.0833 long : 19.9167",
+      "timeStamp":"1659391801",
+      "Weather":"19.91",
+      "Country":"PL",
+      "DayLength":"daylength is 16 H 12 M  "
+   },
+   {
+      "Stadt":"poznan",
+      "Coordinates":"Lat : 52.4069 long : 16.9299",
+      "timeStamp":"1659392103",
+      "Weather":"20.38",
+      "Country":"PL",
+      "DayLength":"daylength is 16 H 29 M  "
+   },
+   {
+      "Stadt":"athens",
+      "Coordinates":"Lat : 37.9795 long : 23.7162",
+      "timeStamp":"1659391920",
+      "Weather":"27.62",
+      "Country":"GR",
+      "DayLength":"daylength is 15 H 5 M  "
+   },
+   {
+      "Stadt":"santorini",
+      "Coordinates":"Lat : 36.3996 long : 25.4208",
+      "timeStamp":"1659392381",
+      "Weather":"25.18",
+      "Country":"GR",
+      "DayLength":"daylength is 14 H 58 M  "
+   },
+   {
+      "Stadt":"split",
+      "Coordinates":"Lat : 43.5089 long : 16.4392",
+      "timeStamp":"1659392382",
+      "Weather":"27.04",
+      "Country":"HR",
+      "DayLength":"daylength is 15 H 31 M  "
+   },
+   {
+      "Stadt":"zadar",
+      "Coordinates":"Lat : 44.1197 long : 15.2422",
+      "timeStamp":"1659392384",
+      "Weather":"22.34",
+      "Country":"HR",
+      "DayLength":"daylength is 15 H 35 M  "
+   },
+   {
+      "Stadt":"pula",
+      "Coordinates":"Lat : 44.8683 long : 13.8481",
+      "timeStamp":"1659392385",
+      "Weather":"20.31",
+      "Country":"HR",
+      "DayLength":"daylength is 15 H 39 M  "
+   },
+   {
+      "Stadt":"oslo",
+      "Coordinates":"Lat : 59.9127 long : 10.7461",
+      "timeStamp":"1659392163",
+      "Weather":"14.06",
+      "Country":"NO",
+      "DayLength":"daylength is 17 H 45 M  "
+   },
+   {
+      "Stadt":"dubrovnik",
+      "Coordinates":"Lat : 42.6481 long : 18.0922",
+      "timeStamp":"1659392190",
+      "Weather":"28.8",
+      "Country":"HR",
+      "DayLength":"daylength is 15 H 27 M  "
+   }
+]
