9f3ccffa56948b6a2b54387094a19399d343bb95
TenForBen <lampard.sibin102@gmail.com>
2022-08-02 23:57:26 +0200

    new list of stadt added

--- wiedersehen/weatherAppTest/ApiUiInt/src/test/java/PoPoi_The_Sailor_Man/appIDEA.java
+++ wiedersehen/weatherAppTest/ApiUiInt/src/test/java/PoPoi_The_Sailor_Man/appIDEA.java
@@ -7,6 +7,8 @@ import static io.restassured.RestAssured.given;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -207,4 +209,98 @@ public class appIDEA extends frameworktest {
 		
 		
 	}
+	
+	@Test
+	public void HillerStrasse ()
+	{
+		
+		Xls_Reader r= new Xls_Reader("H:\\vsos\\TenForBen.github.io\\FPL\\db\\SouthHem.xlsx");
+		//int gw=gameweek;	
+		String xlPath="H:\\vsos\\TenForBen.github.io\\FPL\\db\\SouthHem.xlsx";
+		String snj ="oz";
+		System.out.println("SheetUsed - " +snj);	
+		int  LR =  r.getLastRwoNum(snj);
+		//System.out.println("The last row by method  " + LR);
+		int LRs=LR+1;
+		System.out.println("The last row count is LRs " + LRs);
+		String place;
+		for(int i=2;i<=LRs;i++)
+		{
+			place =r.getCellData(snj, "Stadt",i);
+			System.out.println("Berg Stadt  ist            ~       " + place);
+			getWeatherByCity(place,xlPath,snj,i);
+		}
+	}
+	
+	public void getWeatherByCity (String city,String xlPath,String sheetNamen,int indech)
+	{
+		RestAssured.baseURI ="https://api.openweathermap.org";
+		String jagah =
+		given().log().all().
+		queryParam("q", city)
+		.queryParam("appid", "2b1fd2d7f77ccf1b7de9b441571b39b8")
+		.queryParam("units", "metric").
+		when().get("data/2.5/weather").
+		then().assertThat().statusCode(200).extract().response().asString();
+		System.out.println("\n");
+		JsonPath js = new JsonPath(jagah);
+		String dt = js.getString("dt");
+		int ddt=Integer.parseInt(dt);
+		System.out.println("Raw date is   " + dt);
+		//java.util.Date time = new java.util.Date(dt);
+		java.util.Date time=new java.util.Date((long)ddt*1000);
+		System.out.println(" beutified date is  " + time);
+		String coundry = js.getString("sys.country");
+		String rlat = js.getString("coord.lat");
+		String rlon = js.getString("coord.lon");
+		String asliJagah=js.getString("name");
+		double count = js.getDouble("main.temp");
+		String mainTemp = js.getString("main.temp");
+				Xls_Reader r= new Xls_Reader(xlPath);
+				String  epTimeSR = js.getString("sys.sunrise");
+				int iSR=Integer.parseInt(epTimeSR);
+				String  epTimeSS = js.getString("sys.sunset");
+				int iSS=Integer.parseInt(epTimeSS);
+				int daylength = iSS-iSR;
+				String dl=Duracion(daylength);
+				//System.out.println("                      DayLength -   " +dl + "   | temperature is " + mainTemp   );
+		String snj =sheetNamen;
+		System.out.println("Our  Scrapper sheet- " +snj);
+		int  LR =  r.getLastRwoNum(snj);
+		System.out.println("The last row by method  " + LR);
+		int LRs=LR+2;
+		System.out.println("The last row count is LRs " + LRs);
+				System.out.println("place updated in xl -  " + asliJagah);
+		r.setCellData(snj, "Country", indech, coundry);
+		System.out.println("Country updated in xl -  " + coundry);
+		r.setCellData(snj, "Weather", indech, mainTemp);
+		System.out.println("place updated in xl -  " + mainTemp);
+		//Coordinates
+		String Coordinates ="Lat : " +rlat + " long : " +rlon ;
+		//String Coordinates ="Lat : " +rlat + " long : " +rlon ;
+		r.setCellData(snj, "Coordinates", indech, Coordinates);
+		System.out.println("Coordinates updated in xl -  " + Coordinates);
+		//timeStamp
+		 DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
+         String strDate = dateFormat.format(time);  
+		//DayLength
+		r.setCellData(snj, "timeStamp", indech, dt);
+		r.setCellData(snj, "DayLength", indech, dl);
+		System.out.println("time updated in xl -  " + strDate);
+		
+	}
+	public static String Duracion(int dl) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(dl * 1000L);
+		cal.setTimeZone(TimeZone.getTimeZone("UTC+1"));
+		int daylengthHours = cal.get(Calendar.HOUR_OF_DAY);
+		daylengthHours = daylengthHours + 1;
+		// System.out.println(cal.get(Calendar.HOUR_OF_DAY));//12 hour clock
+		String miren="daylength is " + daylengthHours + " H " + cal.get(Calendar.MINUTE) + " M  ";// 24 hour
+		// clock
+		// return dl;
+		return miren;
+	}
+	
+
 }
