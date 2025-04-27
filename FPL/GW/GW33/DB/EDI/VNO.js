168114acb24d2f52bfa024887c50cd77f294a29e
TenForBenJamin <73674410+TenForBenJamin@users.noreply.github.com>
2025-04-27 18:23:35 +0200

    added gw 34

---
+++ FPL/GW/GW33/DB/EDI/VNO.js
@@ -0,0 +1,188 @@
+var s = [
+  {
+    "Day": "2025-05-01",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-02",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-03",
+    "PriceValue": "128.13",
+    "Arrival": "2025-05-03T13:05:00",
+    "Departure": "2025-05-03T08:45:00"
+  },
+  {
+    "Day": "2025-05-04",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-05",
+    "PriceValue": "128.99",
+    "Arrival": "2025-05-05T13:05:00",
+    "Departure": "2025-05-05T08:45:00"
+  },
+  {
+    "Day": "2025-05-06",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-07",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-08",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-09",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-10",
+    "PriceValue": "355.09",
+    "Arrival": "2025-05-10T13:05:00",
+    "Departure": "2025-05-10T08:45:00"
+  },
+  {
+    "Day": "2025-05-11",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-12",
+    "PriceValue": "119.30",
+    "Arrival": "2025-05-12T13:05:00",
+    "Departure": "2025-05-12T08:45:00"
+  },
+  {
+    "Day": "2025-05-13",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-14",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-15",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-16",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-17",
+    "PriceValue": "203.07",
+    "Arrival": "2025-05-17T13:05:00",
+    "Departure": "2025-05-17T08:45:00"
+  },
+  {
+    "Day": "2025-05-18",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-19",
+    "PriceValue": "71.99",
+    "Arrival": "2025-05-19T13:05:00",
+    "Departure": "2025-05-19T08:45:00"
+  },
+  {
+    "Day": "2025-05-20",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-21",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-22",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-23",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-24",
+    "PriceValue": "133.20",
+    "Arrival": "2025-05-24T13:05:00",
+    "Departure": "2025-05-24T08:45:00"
+  },
+  {
+    "Day": "2025-05-25",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-26",
+    "PriceValue": "133.20",
+    "Arrival": "2025-05-26T13:05:00",
+    "Departure": "2025-05-26T08:45:00"
+  },
+  {
+    "Day": "2025-05-27",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-28",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-29",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-30",
+    "PriceValue": "NA",
+    "Arrival": "fare.ArrivalDate",
+    "Departure": "fare.DepartureDate"
+  },
+  {
+    "Day": "2025-05-31",
+    "PriceValue": "68.05",
+    "Arrival": "2025-05-31T13:05:00",
+    "Departure": "2025-05-31T08:45:00"
+  }
+];
